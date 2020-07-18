dd625e661dfd973493f7557bf5c355b4a29f4dc2
Alex Lelievre <alex@folabs.com>
2020-07-17 18:22:57 -0700

    Make file

--- weather-relay/make.sh
+++ weather-relay/make.sh
@@ -1,2 +1,2 @@
-gcc main.c -I../../tx31u-receiver/ -I../aprs-weather-submit/src/  -lm
+gcc main.c ../aprs-weather-submit/src/aprs-is.c ../aprs-weather-submit/src/aprs-wx.c -I../../tx31u-receiver/ -I../aprs-weather-submit/src/ -lm -o wxrelay
 
