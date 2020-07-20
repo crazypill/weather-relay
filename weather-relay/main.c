e59ba9b6440cf9fc278470969c592b88dde3dd07
Alex Lelievre <alex@folabs.com>
2020-07-20 10:09:53 -0700

    Updated wind format to keep lines aligned

--- weather-relay/main.c
+++ weather-relay/main.c
@@ -298,15 +298,15 @@ void updateStats( const Frame* data, Frame* min, Frame* max, Frame* ave )
 void printFullWeather( const Frame* inst, Frame* min, Frame* max, Frame* ave )
 {
     printTime( false );
-    printf( "     wind[%0.2f°]: %0.2f mph,     gust: %0.2f mph --     temp: %0.2f°F,     humidity: %d%%,     pressure: %g InHg,     int temp: %0.2f°F, rain: %g\n", inst->windDirection, ms2mph( inst->windSpeedMs ), ms2mph( inst->windGustMs ), c2f( inst->tempC ), inst->humidity, (inst->pressure * millibar2inchHg) + kLocalOffsetInHg, c2f( inst->intTempC - kLocalTempErrorC ), 0.0 );
+    printf( "     wind[%05.2f°]: %0.2f mph,     gust: %0.2f mph --     temp: %0.2f°F,     humidity: %d%%,     pressure: %g InHg,     int temp: %0.2f°F, rain: %g\n", inst->windDirection, ms2mph( inst->windSpeedMs ), ms2mph( inst->windGustMs ), c2f( inst->tempC ), inst->humidity, (inst->pressure * millibar2inchHg) + kLocalOffsetInHg, c2f( inst->intTempC - kLocalTempErrorC ), 0.0 );
     printTime( false );
-    printf( " avg wind[%0.2f°]: %0.2f mph, max gust: %0.2f mph -- ave temp: %0.2f°F, ave humidity: %d%%, min pressure: %g InHg  ave int temp: %0.2f°F\n",            ave->windDirection, ms2mph( ave->windSpeedMs ), ms2mph( max->windGustMs ), c2f( ave->tempC ), ave->humidity, (min->pressure * millibar2inchHg) + kLocalOffsetInHg, c2f( ave->intTempC - kLocalTempErrorC ) );
+    printf( " avg wind[%05.2f°]: %0.2f mph, max gust: %0.2f mph -- ave temp: %0.2f°F, ave humidity: %d%%, min pressure: %g InHg  ave int temp: %0.2f°F\n",            ave->windDirection, ms2mph( ave->windSpeedMs ), ms2mph( max->windGustMs ), c2f( ave->tempC ), ave->humidity, (min->pressure * millibar2inchHg) + kLocalOffsetInHg, c2f( ave->intTempC - kLocalTempErrorC ) );
 }
 
 
 void printCurrentWeather( Frame* min, Frame* max, Frame* ave )
 {
-    printf( "Wind[%0.2f°]: %0.2f mph, gust: %0.2f mph, temp: %0.2f°F, humidity: %d%%, pressure: %g InHg, int temp: %0.2f°F, rain: %g\n", ave->windDirection, ms2mph( ave->windSpeedMs ), ms2mph( max->windGustMs ), c2f( ave->tempC ), ave->humidity, (min->pressure * millibar2inchHg) + kLocalOffsetInHg, c2f( ave->intTempC - kLocalTempErrorC ), 0.0 );
+    printf( "Wind[%05.2f°]: %0.2f mph, gust: %0.2f mph, temp: %0.2f°F, humidity: %d%%, pressure: %g InHg, int temp: %0.2f°F, rain: %g\n", ave->windDirection, ms2mph( ave->windSpeedMs ), ms2mph( max->windGustMs ), c2f( ave->tempC ), ave->humidity, (min->pressure * millibar2inchHg) + kLocalOffsetInHg, c2f( ave->intTempC - kLocalTempErrorC ), 0.0 );
 }
 
 
