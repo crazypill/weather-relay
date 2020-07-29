de69642ede879fe65f207c9eaafd469c4ad42d72
Alex Lelievre <alex@folabs.com>
2020-07-29 11:01:05 -0700

    So many errors some times.

--- weather-relay/main.c
+++ weather-relay/main.c
@@ -996,11 +996,11 @@ wx_thread_return_t sendPacket_thread_entry( void* args )
                 break;
             }
             else
-                log_error( "failed to send to APRS-IS, error: %d.  %d of %d retries: %s\n", err, i + 1, s_num_retries, packetToSend );
+                log_error( "failed to send to APRS-IS (%d).  %d of %d retries: %s\n", err, i + 1, s_num_retries, packetToSend );
         }
         
         if( !success )
-            log_error( "NOT sent to APRS-IS, error: %d...%s\n", errno, packetToSend );
+            log_error( "NOT sent to APRS-IS (%d).  %s\n", errno, packetToSend );
     }
     
     free( (void*)packetToSend );
