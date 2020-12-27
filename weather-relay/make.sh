77391ceb9944f4da2791280014b51d8ff208ea42
Alex Lelievre <alex@folabs.com>
2020-12-26 20:31:23 -0800

    Updated make file with new USB verison of rain sensor data.

--- weather-relay/make.sh
+++ weather-relay/make.sh
@@ -1,2 +1,2 @@
-gcc -g main.c wx_thread.c rain_socket.c ../stubs.c ../ax25_pad.c ../kiss_frame.c ../fcs_calc.c ../aprs-weather-submit/src/aprs-is.c ../aprs-weather-submit/src/aprs-wx.c -D__insecure_redirect__ -DKISSUTIL -I. -I.. -I../../tx31u-receiver/ -I../aprs-weather-submit/src/ -lm -o wxrelay -pthread
+gcc -g main.c wx_thread.c rain_socket.c rain_sensor.c ../stubs.c ../ax25_pad.c ../kiss_frame.c ../fcs_calc.c ../aprs-weather-submit/src/aprs-is.c ../aprs-weather-submit/src/aprs-wx.c -D__insecure_redirect__ -DKISSUTIL -I. -I.. -I../../tx31u-receiver/ -I../aprs-weather-submit/src/ -lm -o wxrelay -pthread
 
