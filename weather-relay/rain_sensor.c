94aab9fac920c10f989561ea296553097de4644d
Alex Lelievre <alex@folabs.com>
2020-12-26 20:30:16 -0800

    Add missing files.

---
+++ weather-relay/rain_sensor.c
@@ -0,0 +1,104 @@
+//
+//  rain_sensor.c
+//  wx-relay
+//
+//  Created by Alex Lelievre on 12/26/20.
+//  Copyright © 2020 Far Out Labs, LLC. All rights reserved.
+//
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <sys/time.h>
+#include <sys/select.h>
+#include <sys/termios.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include <signal.h>
+
+#include "main.h"
+#include "wx_thread.h"
+#include "TXDecoderFrame.h"
+
+
+static sig_atomic_t s_quit = 0;
+static sig_atomic_t s_raw_rain_count = 0;
+
+ 
+void process_rain_frame( RainFrame* frame )
+{
+    if( !frame )
+        return;
+    
+    s_raw_rain_count = frame->raw_rain_count;
+    
+#ifdef DEBUG
+    fprintf( stderr, "rain_sensor: raw rain message: %d\n\n", frame->raw_rain_count );
+#endif
+}
+
+
+
+wx_thread_return_t rain_sensor_thread( void* args )
+{
+    if( !args )
+    {
+        log_error( "rain_sensor_thread failed to startup, no device...\n" );
+        wx_thread_return();
+    }
+    
+    int fd = open_serial_port( (const char*)args, B115200 );
+    if( fd < 0 )
+    {
+        log_error( "rain_sensor_thread failed to open serial port: %s...\n", (const char*)args );
+        wx_thread_return();
+    }
+    else
+        log_error( "rain_sensor_thread started: %s...\n", (const char*)args );
+
+    ssize_t result = 0;
+    while( !s_quit )
+    {
+        RainFrame frame;
+
+        result = read( fd, &frame, sizeof( frame ) );
+        if( result == sizeof( frame ) )
+            process_rain_frame( &frame );
+        else if( result )
+        {
+#ifdef DEBUG
+            log_error( " partial incoming rain sensor data %d (%d)\n", result, sizeof( frame )  );
+#endif
+            // we most likely have received a partially transmitted frame, try to do another read now to get the remainder of it
+            sleep( 1 );
+            uint8_t* partialFrame = (uint8_t*)&frame;
+            uint8_t  lastRead = result;
+            result = read( fd, &partialFrame[lastRead], sizeof( frame ) - lastRead );
+            if( result + lastRead == sizeof( frame ) )
+                process_rain_frame( &frame );
+            else
+                log_error( " bad frame size on incoming wx sensor data %d != %d\n", result, sizeof( frame )  );
+        }
+        
+        sleep( 1 );
+    }
+        
+    wx_thread_return();
+}
+
+
+void rain_sensor_thread_quit( void )
+{
+    s_quit = 1;
+}
+
+
+int rain_sensor_raw_count()
+{
+    return s_raw_rain_count;
+}
---
+++ weather-relay/rain_sensor.h
@@ -0,0 +1,21 @@
+//
+//  rain_sensor.h
+//  weather-relay
+//
+//  Created by Alex Lelievre on 12/26/20.
+//  Copyright © 2020 Far Out Labs. All rights reserved.
+//
+
+#ifndef _H_rain_sensor
+#define _H_rain_sensor
+
+#include <stdbool.h>
+#include <stdint.h>
+
+wx_thread_return_t rain_sensor_thread( void* args );
+void               rain_sensor_thread_quit( void );
+int                rain_sensor_raw_count();
+
+
+
+#endif // !_H_rain_sensor
