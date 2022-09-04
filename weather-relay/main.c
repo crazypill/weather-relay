b1f9caf0bf3a50d70447433e8b17dc6280fa0fa2
Alex Lelievre <alex@folabs.com>
2022-09-04 12:51:33 -0700

    Getting closer, number is -nan, need to see why (also look at it in hex).

--- weather-relay/main.c
+++ weather-relay/main.c
@@ -1656,7 +1656,7 @@ void transmit_wx_frame( const Frame* frame )
     float co2 = co2_read_sensor( NULL, NULL );
     
     if( (int32_t)co2 <= 0 )
-        log_error( "transmit_wx_frame: co2_level is zero... %g\n", co2 );
+        log_error( "transmit_wx_frame: co2_level is zero... %g, 0x%lx\n", co2, co2 );
     
     print_wx_for_www( frame, lastHour100sInch, last24Hours100sInch, sinceMidnight100sInch, (int32_t)co2 );
 
